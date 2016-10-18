b07d8a8f53fc661db940d10ad882ba9e73a3ef65
JuanCa958808LDS <juanca8@utp.edu.co>
2016-10-18 13:13:30 -0500

    funcionando

---
+++ fuseRelaunch.cmd
@@ -0,0 +1,1 @@
+-intstyle "ise" -incremental -o "D:/Desktop/procesadorangelica2/procesador2/Tb_PROCESADOR_isim_beh.exe" -prj "D:/Desktop/procesadorangelica2/procesador2/Tb_PROCESADOR_beh.prj" "work.Tb_PROCESADOR" 
